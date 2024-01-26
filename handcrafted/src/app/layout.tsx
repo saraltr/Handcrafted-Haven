428cc65a939a30b68e0cf4730fd37dde7cd9f58e
David Adeogun <adeogundavidt@gmail.com>
2024-01-26 17:49:36 -0500

    Added footer links to layout component

--- handcrafted/src/app/layout.tsx
+++ handcrafted/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./ui/globals.css";
 import { playpen } from '@/app/ui/fonts/fonts';
+import Link from 'next/link';
 
 export const metadata: Metadata = {
   title: {
@@ -17,7 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${playpen.className}`}>{children}</body>
+      <body className={`${playpen.className}`}>
+        {children}
+        <footer>
+          <div className="footer-container">
+            <Link href="/terms-and-conditions">Terms and Conditions</Link>
+            <Link href="/privacy-policy">Privacy Policy</Link>
+            <Link href="/faqs">FAQs</Link>
+          </div>
+        </footer>
+      </body>
     </html>
   );
 }
