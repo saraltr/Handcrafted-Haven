ed08c0caa1324325c5c41c622d717772f2c858fb
sara latorre <saralatorre97@gmail.com>
2024-01-26 04:21:14 +0100

    added fonts and palette

--- handcrafted/src/app/page.tsx
+++ handcrafted/src/app/page.tsx
@@ -1,9 +1,8 @@
-
-
 export default function Page() {
   return(
     <div>
       <h1>Welcome to Handcrafted Haven</h1>
+      <p>Handcrafted Haven is a place for handcrafted goods.</p>
     </div>
   )
 }
---
+++ handcrafted/src/app/ui/fonts/fonts.ts
@@ -0,0 +1,6 @@
+import { Playpen_Sans} from 'next/font/google';
+
+export const playpen = Playpen_Sans({
+    subsets: ['latin'],
+    weight: ['400', '500'],
+})
