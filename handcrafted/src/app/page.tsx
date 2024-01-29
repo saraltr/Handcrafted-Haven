a1fd3c208cd7f152d2290d4e4cc4952cec3f58e4
sara latorre <saralatorre97@gmail.com>
2024-01-29 16:09:50 +0100

    created banner

--- handcrafted/src/app/page.tsx
+++ handcrafted/src/app/page.tsx
@@ -1,8 +1,29 @@
+import React from "react";
+import Banner from "@/app/ui/banner";
+import { ArrowRightIcon } from '@heroicons/react/24/solid'
+import bannerImage from "@/public/images/banner1.jpg";
+
 export default function Page() {
-  return(
+  const nb1 = 35452;
+  const nb2 = 11544;
+
+  return (
     <main>
-      <h1>Welcome to Handcrafted Haven</h1>
-      <p>Handcrafted Haven is a place for handcrafted goods.</p>
+      <Banner
+        bannerImage={bannerImage}
+        title="Welcome to Handcrafted Haven"
+        subtitle="Discover unique and authentic handmade creations!"
+        nb1={nb1.toLocaleString("en-US")}
+        desc1="Orders Delivered"
+        nb2={nb2.toLocaleString("en-US")}
+        desc2="Registered Creators"
+        buttonText={
+          <div className="btnContainer">
+            Register Now
+            <ArrowRightIcon className="arrowIcon" />
+          </div>
+        }
+      />
     </main>
-  )
+  );
 }
---
+++ handcrafted/src/app/ui/banner.tsx
@@ -0,0 +1,47 @@
+import React, { ReactNode } from "react";
+import Image, { StaticImageData } from "next/image";
+import Link from "next/link";
+
+interface BannerProps {
+  bannerImage: StaticImageData;
+  title?: string;
+  subtitle?: string;
+  buttonText?: ReactNode;
+  nb1?: string;
+  desc1?: string;
+  nb2?: string;
+  desc2?: string;
+}
+
+const Banner: React.FC<BannerProps> = ({ bannerImage, title, subtitle, buttonText, nb1, desc1, nb2, desc2 }) => {
+  const hasInfoContent = nb1 || desc1 || nb2 || desc2;
+
+  return (
+    <section className="bannerContainer">
+      <div className="bannerImg">
+        <Image src={bannerImage} alt="Banner Image" />
+      </div>
+      {title && <h1>{title}</h1>}
+      {subtitle && <p className="catchText">{subtitle}</p>}
+      {hasInfoContent && (
+        <div className="info">
+          <div>
+            {nb1 && <h2>{nb1}</h2>}
+            {desc1 && <p>{desc1}</p>}
+          </div>
+          <div>
+            {nb2 && <h2>{nb2}</h2>}
+            {desc2 && <p>{desc2}</p>}
+          </div>
+        </div>
+      )}
+      {buttonText && (
+        <Link href="/login">
+          <button>{buttonText}</button>
+        </Link>
+      )}
+    </section>
+  );
+};
+
+export default Banner;
