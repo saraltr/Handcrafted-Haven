7f687c383a9be888689bd688747dd2f8da538c3b
sara latorre <saralatorre97@gmail.com>
2024-02-01 12:42:33 +0100

    added secret and fixed font error

--- handcrafted/pages/api/auth/[...nextauth].ts
+++ handcrafted/pages/api/auth/[...nextauth].ts
@@ -13,6 +13,7 @@ if(!githubId || !githubSecret){
 }
 
 export const authConfig = {
+    secret: process.env.NEXTAUTH_SECRET,
     providers: [Auth0Provider({
         clientId: process.env.AUTH0_CLIENT_ID as string,
         clientSecret: process.env.AUTH0_CLIENT_SECRET as string,
@@ -36,7 +37,7 @@ export const authConfig = {
             else if (new URL(url).origin === baseUrl) return url
             return baseUrl
         }
-    },
+    }, 
     adapter: PrismaAdapter(prisma) as Adapter
 } satisfies NextAuthOptions;
 
--- handcrafted/src/app/ui/fonts/fonts.ts
+++ handcrafted/src/app/ui/fonts/fonts.ts
@@ -3,4 +3,5 @@ import { Playpen_Sans} from 'next/font/google';
 export const playpen = Playpen_Sans({
     subsets: ['latin'],
     weight: ['400', '500'],
+    display: 'swap' 
 })
