c97c0ede3ca6e24bef7267e9473d78befd3ef62f
sara latorre <saralatorre97@gmail.com>
2024-02-01 11:59:54 +0100

    added missing packages

--- handcrafted/src/app/ui/user.tsx
+++ handcrafted/src/app/ui/user.tsx
@@ -1,32 +1,34 @@
-import { authConfig } from "../../../pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import { LogoutButton } from "./logoutButton";
 import React from 'react';
 import styles from '@/app/ui/user.module.css';
+import { authConfig } from "../../../pages/api/auth/[...nextauth]";
+import defaultImage from "@/public/images/blank-profile-picture.png"
 
 export const User = async () => {
-    const session = await getServerSession(authConfig)
+    const session = await getServerSession(authConfig);
 
-    if(!session?.user) {
+    if (!session?.user) {
         return null;
     }
 
+    const userImage = session.user.image ?? defaultImage; 
+
     return (
-         <div className={styles.userProfileCard}>
+        <div className={styles.userProfileCard}>
             <Image
-                    src={session.user.image}
-                    alt="user avatar"
-                    width={100}
-                    height={100}
-                    className={styles.userAvatar}
-                />
-        {/* <img className={styles.userAvatar} src={session.user.image} alt="user avatar" /> */}
-        <div className={styles.userInfo}>
-            <h2 className={styles.userName}>{session.user.name}</h2>
-            <p className={styles.userEmail}>{session.user.email}</p>
-            <LogoutButton />
+                src={userImage}
+                alt="user avatar"
+                width={100}
+                height={100}
+                className={styles.userAvatar}
+            />
+            <div className={styles.userInfo}>
+                <h2 className={styles.userName}>{session.user.name}</h2>
+                <p className={styles.userEmail}>{session.user.email}</p>
+                <LogoutButton />
+            </div>
         </div>
-        </div>
-    )
-}
+    );
+};
