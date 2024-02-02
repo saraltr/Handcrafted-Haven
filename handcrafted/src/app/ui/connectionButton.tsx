d252aac825a4407f15f0e002053101ce50814b8e
sara latorre <saralatorre97@gmail.com>
2024-02-02 19:24:10 +0100

    added new login btn

---
+++ handcrafted/src/app/ui/connectionButton.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { SessionProvider, useSession, signIn, signOut } from "next-auth/react";
+import { ArrowRightIcon } from '@heroicons/react/24/solid';
+import styles from '@/app/ui/connectionBtn.module.css'
+
+export default function Connection() {
+  return (
+    <SessionProvider>
+      <ConnectionContent />
+    </SessionProvider>
+  );
+}
+
+function ConnectionContent() {
+  const { data: session } = useSession();
+
+  if (session) {
+    const userName = session.user?.name ?? 'User';
+
+    return (
+      <div className={styles.container}>
+        Signed in as {userName} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </div>
+    );
+  }
+
+  return (
+    <div className="btnContainer">
+      <button className="loginBtn" onClick={() => signIn(undefined, { callbackUrl: '/seller' })} >Login Now <ArrowRightIcon className="arrowIcon" /></button>
+    </div>
+  );
+}
