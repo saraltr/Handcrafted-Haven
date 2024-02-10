3fe3e13b72237ff2e38ee237844879685a385e00
sara latorre <saralatorre97@gmail.com>
2024-02-10 12:20:59 +0100

    addjusted nav bar responsiveness

--- handcrafted/src/app/ui/navigation.tsx
+++ handcrafted/src/app/ui/navigation.tsx
@@ -11,26 +11,26 @@ export default function Navigation() {
     };
 
     return (
-        <div className={styles.navbar}>
-            <div className={styles.responsive}>
-                {isOpen ? (
-                    <div className={styles.hamburger} onClick={toggleMenu}>
-                        <span>X</span>
-                    </div>
-                ) : (
-                    <div className={styles.hamburger} onClick={toggleMenu}>
-                        <span>&#9776;</span>
-                    </div>
-                )}
-            </div>
-            <div className={`${styles.navbarLinks} ${isOpen ? styles.show : ''}`}>
-                <ul>
-                    <li><Link href="/">Home</Link></li>
-                    <li><Link href="/catalog">Catalog</Link></li>
-                    <li><Link href="/seller">Profile</Link></li>
-                    <li><Link href="/contact">Contact</Link></li>
-                </ul>
-            </div>
+        <>
+        <div className={styles.responsive}>
+            {isOpen ? (
+                <div className={styles.hamburger} onClick={toggleMenu}>
+                    <span>X</span>
+                </div>
+            ) : (
+                <div className={styles.hamburger} onClick={toggleMenu}>
+                    <span>&#9776;</span>
+                </div>
+            )}
         </div>
+        <div className={`${styles.navbarLinks} ${isOpen ? styles.show : ''}`}>
+            <ul>
+                <li><Link href="/">Home</Link></li>
+                <li><Link href="/catalog">Catalog</Link></li>
+                <li><Link href="/seller">Profile</Link></li>
+                <li><Link href="/contact">Contact</Link></li>
+            </ul>
+        </div>
+        </>
     );
 }
